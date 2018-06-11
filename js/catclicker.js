d592ef22b53d48d1a43875206efc5f6ea3cc6443
Jude Beadle <info@jbeadle.de>
2018-06-11 12:44:24 +0200

    displaying score of clicks on cat

--- js/catclicker.js
+++ js/catclicker.js
@@ -11,22 +11,27 @@ class Cat {
 		this.imageSrc = 'assets/cat' + this.id + '.png';
     	this.image = document.createElement('img');
     	this.image.src = this.imageSrc;
+    	this.scorePanel = document.createElement('p');
+    	this.scorePanelText = "click " + this.name;
+    	this.scorePanel.innerHTML = this.scorePanelText; 
     	this.imageHolder.appendChild(this.image);
   		catContainer.appendChild(this.imageHolder);
-  		console.log(this.imageHolder);
+  		catContainer.appendChild(this.scorePanel);
 	}
 	
-	countClicks(name, numClicks){
+	countClicks(name, numClicks, scorePanel){
 		numClicks = this.numClicks;
 		name = this.name;
+		scorePanel = this.scorePanel;
 		this.imageHolder.addEventListener('click', function () {
 		    numClicks ++;
-			updateCounter(name, numClicks);
+			updateCounter(name, numClicks, scorePanel);
 		}, false);
 	}
 }
 
 let cat1, cat2;
+
 function createCats(catContainer, imageHolder){
 	cat1 = new Cat("Moggie", 1);
 	cat1.addCatHTML(catContainer);
@@ -35,13 +40,15 @@ function createCats(catContainer, imageHolder){
 	cat2.addCatHTML(catContainer);
 	cat2.countClicks(this.imageHolder);
 }
-function updateCounter(name, numClicks){
-	console.log(name + numClicks);
+function updateCounter(name, numClicks, scorePanel){
+	if (numClicks === 1){
+		scorePanel.innerHTML = 'You\'ve clicked ' + name + ' ' + numClicks + ' time';
+	} else {
+		scorePanel.innerHTML = 'You\'ve clicked ' + name + ' ' + numClicks + ' times';
+	}
 }
 
 window.addEventListener('load',function(){
-
-
 	// use dom elements
     const container = document.querySelector('.container');
 	createCats(container);	
