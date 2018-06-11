008a7b10519df4c01d6afea0c902cc83a00298eb
Jude Beadle <info@jbeadle.de>
2018-06-11 12:24:48 +0200

    adding eventlistener for clicks

--- js/catclicker.js
+++ js/catclicker.js
@@ -1,49 +1,60 @@
 class Cat {
-	constructor(name, numClicks = 0, imageSrc, id){
+	constructor(name, id){
 		this.name = name;
 		this.id = id;
 		this.numClicks = 0;
+		this.imageHolder = document.createElement('div');
+		this.imageHolder.classList.add('image-holder', 'image' + this.id);
 	}
 
 	addImage(catContainer){
+		
 		this.imageSrc = 'assets/cat' + this.id + '.png';
-		this.imageHolder = document.createElement('div');
-    	this.imageHolder.classList.add('image-holder', 'image' + this.id);
     	this.image = document.createElement('img');
     	this.image.src = this.imageSrc;
     	this.imageHolder.appendChild(this.image);
   		catContainer.appendChild(this.imageHolder);
+  		console.log(this.imageHolder);
 	}
 	
-	countClicks(){
-		addEventListener('click', function () {
-		    this.numClicks ++;
-			updateCounter(this.numClicks);
+
+	countClicks(name, numClicks){
+		numClicks = this.numClicks;
+		name = this.name;
+		this.imageHolder.addEventListener('click', function () {
+		    numClicks ++;
+			updateCounter(name, numClicks);
 		}, false);
 	}
+
+	
+	
+	
 }
 
 let cat1, cat2;
-function createCats(catContainer){
-	cat1 = new Cat;
-	cat1.name = "Moggie";
-	cat1.id = 1;
+function createCats(catContainer, imageHolder){
+	cat1 = new Cat("Moggie", 1);
 	cat1.addImage(catContainer);
-
-	cat2 = new Cat;
-	cat2.name = "Paws";
-	cat2.id = 2;
+	cat1.countClicks(name, this.numClicks);
+	cat2 = new Cat("Paws", 2);
 	cat2.addImage(catContainer);
+	cat2.countClicks(this.imageHolder);
 }
+function updateCounter(name, numClicks){
+	console.log(name + numClicks);
+}
+
+
 
 window.addEventListener('load',function(){
 
-	
 
 	// use dom elements
     const container = document.querySelector('.container');
-    	
 	createCats(container);
+	
+	
 
 })
 
